3a63c4660dfd5af4177a1be2ecf2e466f92051f4
Marek Vaníček <marcovindy@gmail.com>
2023-06-28 08:17:03 +0200

    Initial commit

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,3 @@
+export const Dashboard = () => {
+    return <h2>DashBoard</h2>
+}
---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from '@reduxjs/toolkit'
+
+export const rootReducer = combineReducers({
+    //your TODO should go here
+})
+
+export type RootState = ReturnType<typeof rootReducer>
