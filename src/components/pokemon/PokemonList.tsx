551014c0ecd9081ed8f78d3597fb0d8b206b6c86
Marek Vaníček <marcovindy@gmail.com>
2023-06-29 12:12:42 +0200

    Create loadMoreButton

--- src/components/pokemon/PokemonList.tsx
+++ src/components/pokemon/PokemonList.tsx
@@ -3,6 +3,7 @@ import { fetchPokemonList, fetchPokemonById } from '@/services/pokemonApi'
 import styled from 'styled-components'
 import Pokemon from './interface/pokemon.interface'
 import PokemonCard from './PokemonCard'
+import { Button } from '@mui/material'
 
 const PokemonListContainer = styled.div`
     display: flex;
@@ -10,10 +11,20 @@ const PokemonListContainer = styled.div`
     justify-content: center;
 `
 
+const LoadMoreButton = styled(Button)`
+    margin-bottom: 1rem;
+`
+
+const LoadMoreButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 2rem;
+`
+
 const PokemonList: React.FC = () => {
     const [pokemons, setPokemons] = useState<Pokemon[]>([])
     const [pokemonOffset, setPokemonOffset] = useState<number>(1)
-    const [pokemonLimit, setPokemonLimit] = useState<number>(10)
+    const [pokemonLimit, setPokemonLimit] = useState<number>(20)
 
     const fetchPokemons = async () => {
         const pokemonData: Pokemon[] = []
@@ -21,7 +32,12 @@ const PokemonList: React.FC = () => {
             const response = await fetchPokemonById(i)
             pokemonData.push(response)
         }
-        setPokemons(pokemonData)
+        setPokemons((prevPokemons) => [...prevPokemons, ...pokemonData])
+    }
+
+    const handleLoadMore = () => {
+        setPokemonOffset((prevOffset) => prevOffset + 20)
+        setPokemonLimit((prevLimit) => prevLimit + 20)
     }
 
     useEffect(() => {
@@ -36,6 +52,11 @@ const PokemonList: React.FC = () => {
                     <PokemonCard key={pokemon.id} pokemon={pokemon} />
                 ))}
             </PokemonListContainer>
+            <LoadMoreButtonContainer>
+                <LoadMoreButton onClick={handleLoadMore} variant="contained" color="primary">
+                    Načíst další pokémony
+                </LoadMoreButton>
+            </LoadMoreButtonContainer>
         </div>
     )
 }
